efbc211f6074e1a5b5788deca8dd75ebb80c4ada
Mike Olthoff <mike@olthoff.ca>
2020-07-15 11:59:44 -0700

    Extruder PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -491,9 +491,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 22.86
+  #define DEFAULT_Ki 2.23
+  #define DEFAULT_Kd 58.65
 
 #endif // PIDTEMP
 
