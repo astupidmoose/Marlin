67b38cd91e8065146a558e5c6e653e8a77081772
Mike Olthoff <mike@olthoff.ca>
2020-08-21 10:17:33 -0700

    Update bed size for offsets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1129,10 +1129,10 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -5
-#define Y_MIN_POS -6
+#define Y_MIN_POS -10
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS (X_BED_SIZE -5)
+#define Y_MAX_POS (Y_BED_SIZE -10)
 #define Z_MAX_POS 250
 
 /**
